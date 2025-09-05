afd6bf9365f2e1c7b5e7f75b68e70a2d99f5fd57
shambihamisi <shambi.hamisi@gmail.com>
2025-09-05 07:09:02 -0700

    changes

--- alx-react-app -new/src/App.jsx
+++ alx-react-app -new/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 import './App.css'
 import React from "react";
-import UserProfile from "./UserProfile";
-import Header from "./Header";
-import MainContent from './MainContent';
-import Footer from './Footer';
+import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
