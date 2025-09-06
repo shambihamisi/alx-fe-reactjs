a13cee40740445f694eaafa1666dee39ea438658
shambihamisi <shambi.hamisi@gmail.com>
2025-09-05 17:40:55 -0700

    changes

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+import React from "react";
+import UserProfile from "./components/UserProfile";
+import Header from "./components/Header";
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Counter />
+      <h1>User Profiles</h1>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      <UserProfile name="Bob" age="30" bio="Avid traveler and foodie" />
+      <UserProfile name="Charlie" age="28" bio="Enjoys coding and gaming" />
+       <Footer />
+    </div>
+  );
+}
+
+export default App;
