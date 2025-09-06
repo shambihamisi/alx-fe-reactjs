367bf167a292c815a0c39129396700ee757200fe
shambihamisi <shambi.hamisi@gmail.com>
2025-09-05 17:25:08 -0700

    changes

--- alx-react-app -new/src/App.jsx
+++ alx-react-app -new/src/App.jsx
@@ -5,12 +5,14 @@ import UserProfile from "./components/UserProfile";
 import Header from "./components/Header";
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div>
       <Header />
       <MainContent />
+      <Counter />
       <h1>User Profiles</h1>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <UserProfile name="Bob" age="30" bio="Avid traveler and foodie" />
