59a6bbe9ada6955540efb77f7c1c77af14ce034a
shambihamisi <shambi.hamisi@gmail.com>
2025-09-28 12:09:40 -0700

    changes

--- tailwind-react-integration/src/components/Userprofile.jsx
+++ tailwind-react-integration/src/components/Userprofile.jsx
@@ -8,8 +8,7 @@ function UserProfile() {
       />
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base">
-        Developer at Example Co. Loves to write code and explore new
-        technologies.
+        Developer at Example Co. Loves to write code and explore new technologies.
       </p>
     </div>
   );
